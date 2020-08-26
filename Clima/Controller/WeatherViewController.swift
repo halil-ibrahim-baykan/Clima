d11b42d501854079ebe7e7d23ef6875fd1f2b617
Halil <halilibrahimbaykan@gmail.com>
2020-08-26 14:54:20 +0200

    start project

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by halil ibrahim baykan on 21/08/2020.
+//  Copyright © 2020 halil ibrahim baykan. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
